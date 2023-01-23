04605030b280c10c931288b579e00bba0a9b6325
Milena Maeda <milenaminekawa@gmail.com>
2023-01-23 18:03:31 -0300

    Rails_Watch_List_Back-end_Completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :lists, only: %i[index new create show] do
+    resources :bookmarks, only: %i[new create]
+    # "%i" => Array de symbols
+  end
+  delete 'bookmarks/:id', to: 'bookmarks#destroy', as: 'bookmark'
 end
